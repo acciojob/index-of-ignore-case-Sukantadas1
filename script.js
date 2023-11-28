e1e8452485d55f0f7c4d370658a54870a382d2f6
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 10:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+    // Check for edge cases (empty strings)
+  if (str === '' || subStr === '') {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  // Find the index of the first occurrence of subStr in str
+  const index = lowerStr.indexOf(lowerSubStr);
+
+  return index;
 }
 
 // Please do not change the code below
